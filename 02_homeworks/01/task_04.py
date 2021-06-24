2bc3d65685be4eb52b3c38f76b9ea6e60bd3d38b
Стрекаловский Владимир Николаевич <v.strekalovskiy@puls.ru>
2021-06-24 09:34:51 +0300

    added 1 homework

---
+++ 02_homeworks/01/task_04.py
@@ -0,0 +1,25 @@
+'''
+4. Пользователь вводит целое 
+положительное число.
+Найдите самую большую цифру в числе.
+Для решения используйте цикл while и 
+арифметические операции.
+'''
+
+input_num = input('Please, enter any number:\n>>> ')
+
+count_numbers = len(input_num)
+
+curr_num = 0
+
+bigest_number = 0
+
+while curr_num != count_numbers:
+    current_number = int(input_num[curr_num])
+
+    if bigest_number - current_number < 0:
+        bigest_number = current_number
+        
+    curr_num += 1
+
+print(f'The biggest number is: {bigest_number}')

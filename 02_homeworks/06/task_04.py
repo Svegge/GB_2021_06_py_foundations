7c5a949f9f640be0f928fd7a1928f8856f7c0448
Стрекаловский Владимир Николаевич <v.strekalovskiy@puls.ru>
2021-07-14 15:47:51 +0300

    added 4-5 tasks of hw_06

--- 02_homeworks/06/task_04.py
+++ 02_homeworks/06/task_04.py
@@ -12,18 +12,20 @@
    результат.
 """
 
+
 class Car():
-    def __init__(self, speed, color, name, is_police) -> None:
+    is_police = 0
+    name = 'Car'
+
+    def __init__(self, speed, color) -> None:
         self.speed = speed
         self.color = color
-        self.name = name
-        self.is_police = is_police
-    
+
     def go(self):
-        print('Car runs')
+        print(f'{self.name} runs')
 
     def stop(self):
-        print('Car stops')
+        print(f'{self.name} stops')
 
     def turn(self, direction):
         dic_directions = {
@@ -32,27 +34,103 @@ class Car():
             'a': 'left',
             'd': 'right',
         }
-        print(f'Car turns {dic_directions[direction]}')
-    
+        print(f'{self.name} turns {dic_directions[direction]}')
+
     def show_speed(self):
         print(self.speed)
 
+
 class TownCar(Car):
-    is_police = 0
-    name = 'town car'
+    """
+    не понял разницу между передачей параметров через super()
+    и и передачей через self. в примере ниже:
+    """
+
+    def __init__(self, speed, color):
+        # super().__init__(speed, color)
+        self.speed = speed
+        self.color = color
+        self.name = 'Town Car'
 
     def show_speed(self):
         if self.speed > 60:
             print('Exceed speed limit!')
 
-class SportCar(Car):
-    def __init__(self, speed, color, name, is_police) -> None:
-        super().__init__(speed, color, name, is_police)
-        self.is_police = 0
-        self.name = 'sport car'
 
-car_one = TownCar(150, 'green')
-car_one.show_speed()
-print(car_one.name, car_one.color, car_one.is_police, car_one.speed)
-# TownCar, SportCar,
-#    WorkCar, PoliceCar
+class SportCar(Car):
+
+    def __init__(self, speed, color):
+        super().__init__(speed, color)
+        self.name = 'Sport Car'
+
+
+class PoliceCar(Car):
+
+    def __init__(self, speed, color):
+        super().__init__(speed, color)
+        self.name = 'Police Car'
+        self.is_police = 1
+
+
+class WorkCar(Car):
+    """
+    не понял разницу между передачей параметров через super()
+    и и передачей через self. в примере ниже:
+    """
+
+    def __init__(self, speed, color):
+        # super().__init__(speed, color)
+        self.speed = speed
+        self.color = color
+        self.name = 'Work Car'
+
+    def show_speed(self):
+        if self.speed > 40:
+            print('Exceed speed limit!')
+
+
+if __name__ == '__main__':
+    car_one = TownCar(150, 'green')
+    car_one.show_speed()
+    print(
+        car_one.name,
+        car_one.color,
+        car_one.is_police,
+        car_one.speed,
+        '\n',
+        '-' * 10)
+
+    car_two = SportCar(200, 'yellow')
+    car_two.show_speed()
+    print(
+        car_two.name,
+        car_two.color,
+        car_two.is_police,
+        car_two.speed,
+        '\n',
+        '-' * 10)
+
+    car_three = PoliceCar(90, 'black')
+    car_three.show_speed()
+    print(
+        car_three.name,
+        car_three.color,
+        car_three.is_police,
+        car_three.speed,
+        '\n',
+        '-' * 10)
+
+    car_four = WorkCar(90, 'brown')
+    car_four.show_speed()
+    print(
+        car_four.name,
+        car_four.color,
+        car_four.is_police,
+        car_four.speed,
+        '\n',
+        '-' * 10)
+
+    car_four.go()
+    car_four.stop()
+    car_four.turn('a')
+    car_four.turn('w')
